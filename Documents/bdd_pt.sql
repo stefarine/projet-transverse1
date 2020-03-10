981ec6a6a9dd7f7509923ba07bf0095d886f5d10
JuanKerber <juanfke@hotmail.com>
2020-03-10 17:37:13 +0100

    bdd provisoire

---
+++ Documents/bdd_pt.sql
@@ -0,0 +1,147 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le :  mar. 10 mars 2020 à 17:35
+-- Version du serveur :  10.1.37-MariaDB
+-- Version de PHP :  7.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `test`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `classement`
+--
+
+CREATE TABLE `classement` (
+  `id_joueur` int(11) NOT NULL,
+  `id_partie` int(11) NOT NULL,
+  `place` set('1','2','3','4') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `joueur`
+--
+
+CREATE TABLE `joueur` (
+  `id_joueur` int(11) NOT NULL,
+  `pseudo` varchar(45) NOT NULL,
+  `mdp` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `partie`
+--
+
+CREATE TABLE `partie` (
+  `id_partie` int(11) NOT NULL,
+  `nom` varchar(45) NOT NULL,
+  `mdp` varchar(45) NOT NULL,
+  `date` datetime NOT NULL,
+  `type` set('public','prive') NOT NULL,
+  `etat` set('init','playing','ended') NOT NULL,
+  `id_tournoi` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `tournoi`
+--
+
+CREATE TABLE `tournoi` (
+  `id_tournoi` int(11) NOT NULL,
+  `nom` varchar(45) NOT NULL,
+  `mdp` varchar(45) NOT NULL,
+  `date` datetime NOT NULL,
+  `type` set('public','prive') NOT NULL,
+  `etat` set('init','playing','ended') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `classement`
+--
+ALTER TABLE `classement`
+  ADD PRIMARY KEY (`id_joueur`,`id_partie`),
+  ADD KEY `fkIdx_54` (`id_joueur`),
+  ADD KEY `fkIdx_57` (`id_partie`);
+
+--
+-- Index pour la table `joueur`
+--
+ALTER TABLE `joueur`
+  ADD PRIMARY KEY (`id_joueur`);
+
+--
+-- Index pour la table `partie`
+--
+ALTER TABLE `partie`
+  ADD PRIMARY KEY (`id_partie`),
+  ADD KEY `fkIdx_68` (`id_tournoi`);
+
+--
+-- Index pour la table `tournoi`
+--
+ALTER TABLE `tournoi`
+  ADD PRIMARY KEY (`id_tournoi`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `joueur`
+--
+ALTER TABLE `joueur`
+  MODIFY `id_joueur` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `partie`
+--
+ALTER TABLE `partie`
+  MODIFY `id_partie` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `classement`
+--
+ALTER TABLE `classement`
+  ADD CONSTRAINT `FK_54` FOREIGN KEY (`id_joueur`) REFERENCES `joueur` (`id_joueur`),
+  ADD CONSTRAINT `FK_57` FOREIGN KEY (`id_partie`) REFERENCES `partie` (`id_partie`);
+
+--
+-- Contraintes pour la table `partie`
+--
+ALTER TABLE `partie`
+  ADD CONSTRAINT `FK_68` FOREIGN KEY (`id_tournoi`) REFERENCES `tournoi` (`id_tournoi`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
